ffdd4906b0820af70906d7784bbc036177329f87
tatolate <warotasyobnn@gmail.com>
2025-04-19 08:52:12 +0000

    原型

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Page1 from './page/Page1.vue';
+import Page2 from './page/Page2.vue';
+import Page3 from './page/Page3.vue';
+import Page4 from './page/Page4.vue';
+const page_number = ref(1)
+</script>
+
+<template>
+  <Page1 @nextpage="page_number++" v-if="page_number == 1" />
+  <Page2 @nextpage="page_number++" v-if="page_number == 2" />
+  <Page3 @nextpage="page_number++" v-if="page_number == 3" />
+  <Page4 @nextpage="page_number = 1" v-if="page_number == 4" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/page/Page2.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>2ページ目</h1>
+
+  <button @click="$emit('nextpage')">2ページ目</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
