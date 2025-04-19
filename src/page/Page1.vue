8504b0c261d797b71945271c35b53b8be100a38e
tatolate <warotasyobnn@gmail.com>
2025-04-19 10:49:14 +0000

    更新

--- src/page/Page1.vue
+++ src/page/Page1.vue
@@ -3,13 +3,51 @@
 </script>
 
 <template>
-  <h1>1ページ目</h1>
+  <header>
+    <div class="header_top">
+      <img src="https://placehold.jp/37x37.png" class="icon1">
+      <img src="https://placehold.jp/37x37.png" class="icon2">
+      <img src="https://placehold.jp/37x37.png" class="icon3">
+      <img src="https://placehold.jp/237x68.png" class="logo">
+    </div>
+    <div>
+      <h2>a</h2>
+    </div>
+  </header>
 
   <button @click="$emit('nextpage')">2ページ目</button>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  header{
+    position: relative;
+    height: 135px;
+    background-color: aqua;
+  }
+
+  .icon1{
+    position: absolute;
+    top: 37px;
+    left: 5px;
+  }
+
+  .icon2{
+    position: absolute;
+    top: 37px;
+    left: 47px;
+  }
+
+  .icon3{
+    position: absolute;
+    top: 37px;
+    left: 89px;
+  }
+
+  .logo{
+    float: right;
+    position: absolute;
+    top: 16px;
+    right: 17px;
+  }
+
 </style>
