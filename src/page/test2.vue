a29a0c2f778df89f74431509ea5bd579f30947c1
tatolate <warotasyobnn@gmail.com>
2025-04-24 08:23:43 +0000

    change teset2.vue

--- src/page/test2.vue
+++ src/page/test2.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import vibration from "../components/tachihara.vue";
+    import vibration from "../components/WholeNoise.vue";
     import Header from "../components/Headers/Artwork.vue"
 </script>
 
