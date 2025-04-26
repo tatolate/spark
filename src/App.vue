c3cc12787ea3eec90da60a9cfc030cfd932ffdcc
shoji1021 <shoji.masato@it-mirai-h.ibk.ed.jp>
2025-04-26 12:37:14 +0900

    競合部分の修正

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,8 @@ import Page1 from './page/Page1.vue';
 import Page2 from './page/Page2.vue';
 import Page3 from './page/Page3.vue';
 import Page4 from './page/Page4.vue';
-<<<<                  <<< HEAD
 import PageLive from './page/PageLive.vue';
-const page_number = ref(100)
-document.cookie="experience=true; path=/";
-=======
+
 
 const page_number = ref(1);
 const contentData = ref<ContentDataType>({
@@ -30,7 +27,6 @@ onMounted(() => {
       console.error('API取得エラー:', error);
     });
 });
->>>>>>> 5d393935a5ec426e672bcb2377cdd17098bcd182
 </script>
 
 <template>
