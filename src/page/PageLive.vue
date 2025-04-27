31f0ae0b1fd6384ef67a13932576a07c02152add
shoji1021 <shoji.masato@it-mirai-h.ibk.ed.jp>
2025-04-27 18:44:28 +0900

    PageLiveにLiveHeaderの追加

--- src/page/PageLive.vue
+++ src/page/PageLive.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import FaceDetection from '../../src/components/FaceDetection.vue';
+import LiveHeader from "../components/Headers/liveheaders.vue";
 
 
 </script>
 
 <template>
+  <LiveHeader :inside="true"/>
   <FaceDetection />
   <button @click="$emit('nextpage')">2ページ目</button>
 </template>
